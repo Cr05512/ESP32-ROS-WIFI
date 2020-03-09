c564c70bf943b52faeae98ed0635d30bc1c222bb
cr05512 <alessandrodortenzio@hotmail.it>
2020-03-09 19:55:29 +0100

    Added MPU6050 over WIFI transmission

--- main/main.ino
+++ main/main.ino
@@ -1,47 +1,200 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps_V6_12.h"
 #include <WiFi.h>
 #include <ros.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Int16.h>
-#include <std_msgs/Float64.h>
 #include <ros/time.h>
-
-#define DEBUG 1
+#include <sensor_msgs/Imu.h>
+#include <Time.h>
 
 // ROS nodes //
 ros::NodeHandle nh;
-std_msgs::String str_msg;
-std_msgs::Int16 int_msg;
+sensor_msgs::Imu imuData;
 
-ros::Publisher pub_string("/esp32/chatter", &str_msg);
+ros::Publisher pub_imuData("/esp32/imuData", &imuData);
+char base_link[] = "/base_link";
+uint16_t freqPub = 20;  //Publishing frequency
+uint16_t waitTime = (uint16_t) 1000.0 / freqPub;
+unsigned long t0, t1;
 
-const char* ssid = "WebCube4-4NN5";
-const char* password = "ELG67HAH";
-IPAddress server(192,168,1,107);      // Set the rosserial socket ROSCORE SERVER IP address
+//WIFI Setup
+//const char* ssid = "WebCube4-4NN5";
+const char* ssid = "AndroidAPC";
+//const char* password = "ELG67HAH";
+const char* password = "robotica18";
+IPAddress server(192,168,43,249);      // Set the rosserial socket ROSCORE SERVER IP address
 const uint16_t serverPort = 11411;    // Set the rosserial socket server port
-
 void setupWiFi();                  // connect to ROS server as as a client
+#define DEBUG 1
+
+#define degToRad(angleInDegrees) ((angleInDegrees) * M_PI / 180.0)
+#define radToDeg(angleInRadians) ((angleInRadians) * 180.0 / M_PI)
+
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 accel;         // [x, y, z]            accel sensor measurements
+VectorFloat gravity;
+VectorInt16 gyro;         // [x, y, z]            gyro sensor measurements
+int16_t gx, gy, gz;
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
 
 void setup() {
-  if(DEBUG) Serial.begin(115200);
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin(21, 22, 400000);
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+
+  // initialize serial communication
+  // (115200 chosen because it is required for Teapot Demo output, but it's
+  // really up to you depending on your project)
+  Serial.begin(115200);
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+  // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+  // the baud timing being too misaligned with processor ticks. You must use
+  // 38400 or slower in these cases, or use some kind of external separate
+  // crystal solution for the UART timer.
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // The code starts without waiting
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(51);
+  mpu.setYGyroOffset(8);
+  mpu.setZGyroOffset(21);
+  mpu.setXAccelOffset(1150);
+  mpu.setYAccelOffset(-50);
+  mpu.setZAccelOffset(1060);
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    Serial.println();
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (ESP32 pin 32"));
+    pinMode(32, INPUT_PULLUP);
+    Serial.println(F(")..."));
+    attachInterrupt(32, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+
   setupWiFi();
   nh.getHardware()->setConnection(server, serverPort);
   nh.initNode();
-  nh.advertise(pub_string);
-  str_msg.data = "Test";
+  imuData.header.frame_id = base_link;
+  nh.advertise(pub_imuData);
+  t1 = millis();
+  t0 = t1;
 }
 
-ros::Time current_time = nh.now();
-ros::Time last_time = current_time;
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
 
 void loop() {
-  if (nh.connected()) {
-    pub_string.publish(&str_msg);
+  if(nh.connected()){
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+    // read a packet from FIFO
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      imuData.orientation.w = q.w;
+      imuData.orientation.x = q.x;
+      imuData.orientation.y = q.y;
+      imuData.orientation.z = q.z;
+
+      mpu.getRotation(&gx, &gy, &gz);
+      imuData.angular_velocity.x = degToRad(gx / 131.0);
+      imuData.angular_velocity.y = degToRad(gx / 131.0);
+      imuData.angular_velocity.z = degToRad(gz / 131.0);
+
+      mpu.dmpGetGravity(&gravity, &q);
+      imuData.linear_acceleration.x = gravity.x;
+      imuData.linear_acceleration.y = gravity.y;
+      imuData.linear_acceleration.z = gravity.z;
   
+    }
+  }
+  t1 = millis();
+  if(t1 - t0 > waitTime){
+    imuData.header.stamp = nh.now();
+    pub_imuData.publish(&imuData);
+    t0 = millis();
   }
   nh.spinOnce();
-  // Loop aprox. every  
-  delay(200);  // milliseconds
-
 }
 
 void setupWiFi() {                    // connect to ROS server as as a client
