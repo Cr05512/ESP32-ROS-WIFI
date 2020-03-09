126fed203f76c4c9d901a46c4776db4efcbe29a5
Alessandro D'Ortenzio <33663162+Cr05512@users.noreply.github.com>
2020-03-09 21:47:38 +0100

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -56,21 +56,10 @@ VectorFloat gravity;
 VectorInt16 gyro;         // [x, y, z]            gyro sensor measurements
 int16_t gx, gy, gz;
 
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
   mpuInterrupt = true;
 }
-
-
-
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
 
 void setup() {
   // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -80,40 +69,25 @@ void setup() {
     Fastwire::setup(400, true);
   #endif
 
-  // initialize serial communication
-  // (115200 chosen because it is required for Teapot Demo output, but it's
-  // really up to you depending on your project)
   Serial.begin(115200);
-  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+  while (!Serial);
 
-  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
-  // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
-  // the baud timing being too misaligned with processor ticks. You must use
-  // 38400 or slower in these cases, or use some kind of external separate
-  // crystal solution for the UART timer.
-
-  // initialize device
   Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
 
-  // verify connection
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-  // The code starts without waiting
-
-  // load and configure the DMP
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
 
-  // supply your own gyro offsets here, scaled for min sensitivity
   mpu.setXGyroOffset(51);
   mpu.setYGyroOffset(8);
   mpu.setZGyroOffset(21);
   mpu.setXAccelOffset(1150);
   mpu.setYAccelOffset(-50);
   mpu.setZAccelOffset(1060);
-  // make sure it worked (returns 0 if so)
+ 
   if (devStatus == 0) {
     // Calibration Time: generate offsets and calibrate our MPU6050
     mpu.CalibrateAccel(6);
@@ -138,10 +112,6 @@ void setup() {
     // get expected DMP packet size for later comparison
     packetSize = mpu.dmpGetFIFOPacketSize();
   } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
     Serial.print(F("DMP Initialization failed (code "));
     Serial.print(devStatus);
     Serial.println(F(")"));
@@ -155,13 +125,6 @@ void setup() {
   t1 = millis();
   t0 = t1;
 }
-
-
-
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
-
 
 void loop() {
   if(nh.connected()){
@@ -197,7 +160,7 @@ void loop() {
   nh.spinOnce();
 }
 
-void setupWiFi() {                    // connect to ROS server as as a client
+void setupWiFi() { 
   if(DEBUG){
     Serial.print("Connecting to ");
     Serial.println(ssid);
